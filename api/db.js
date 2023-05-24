6c71ed62541909f9a12f9ab03e271db13a1ae072
DiogoSantus <83404802+DiogoSantus@users.noreply.github.com>
2023-05-24 23:26:07 +0100

    mudança da password da base de dados

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "123@Dionic",
+    password: "diogosantos123",
     database: "crud"
 })
